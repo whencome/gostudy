cd916ed1e48b94e5b8c2ed2a0f23ae5eddd167dd
whencome <twqzy323@163.com>
2023-05-08 23:07:24 +0800

    add verctor demo

---
+++ Code/036-vector-01/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/floats" // 本示例的向量操作使用floats实现
+)
+
+func main() {
+	// 使用浮点数初始化两个向量
+	vectorA := []float64{11.0, 5.2, -1.3}
+	vectorB := []float64{-7.2, 4.2, 5.1}
+
+	// 计算两个向量的点积
+	dotProduct := floats.Dot(vectorA, vectorB)
+	fmt.Printf("the dot product of A and B is : %.2f\n", dotProduct)
+
+	// 将向量个各元素扩展1.5倍
+	floats.Scale(1.5, vectorA)
+	fmt.Printf("Scaling A by 1.5v gives: %v\n", vectorA)
+
+	// 求向量B的欧几里德范数
+	normB := floats.Norm(vectorB, 2)
+	fmt.Printf("The norm/length of B is: %0.2f\n", normB)
+}
---
+++ Code/037-vector-02/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/mat" // 本示例的向量操作使用mat实现
+)
+
+func main() {
+	// 使用浮点数初始化两个向量
+	vectorA := mat.NewVecDense(3, []float64{11.0, 5.2, -1.3})
+	vectorB := mat.NewVecDense(3, []float64{-7.2, 4.2, 5.1})
+
+	// 计算两个向量的点积
+	dotProduct := mat.Dot(vectorA, vectorB)
+	fmt.Printf("the dot product of A and B is : %.2f\n", dotProduct)
+
+	// 将向量个各元素扩展1.5倍
+	vectorA.ScaleVec(1.5, vectorA)
+	fmt.Printf("Scaling A by 1.5v gives: %v\n", vectorA)
+
+	// 求向量B的欧几里德范数
+	normB := blas64.Nrm2(vectorB.RawVector())
+	fmt.Printf("The norm/length of B is: %0.2f\n", normB)
+}
